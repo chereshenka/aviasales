c1c1be4f9bebf0ff1ae68c9bda7fc1ceb7c76704
chereshenka <ladya.ferz@gmail.com>
2022-11-30 16:47:55 +0300

    redux try understanding, first entry

--- src/components/list-items/list-items.js
+++ src/components/list-items/list-items.js
@@ -1,5 +1,5 @@
 import uniqid from "uniqid";
-import { useState } from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { format, parseISO } from "date-fns";
 import { Space, Spin } from "antd";
--- src/components/tabs/tabs.js
+++ src/components/tabs/tabs.js
@@ -1,5 +1,5 @@
 import { useSelector, useDispatch } from "react-redux";
-
+import React from "react";
 import {
   sortTicketsByPrice,
   sortTicketsByTime,
@@ -19,35 +19,39 @@ const Tab = () => {
   };
 
   return (
-    <div className={styles.tab__list} onClick={onChangeTab}>
-      <button
-        id="cheap"
-        onClick={() => dispatch(sortTicketsByPrice(state))}
-        className={
-          styles.tab__cheap + " " + (tab === "cheap" ? styles.active : null)
-        }
-      >
-        Самый дешевый
-      </button>
-      <button
-        id="fast"
-        onClick={() => dispatch(sortTicketsByTime(state))}
-        className={
-          styles.tab__fast + " " + (tab === "fast" ? styles.active : null)
-        }
-      >
-        Самый быстрый
-      </button>
-      <button
-        id="optimal"
-        className={
-          styles.tab__optimal + " " + (tab === "optimal" ? styles.active : null)
-        }
-        disabled
-      >
-        Оптимальный
-      </button>
-    </div>
+    <>
+      <div className={styles.tab__list} onClick={onChangeTab}>
+        <button
+          id="cheap"
+          onClick={() => dispatch(sortTicketsByPrice(state))}
+          className={
+            styles.tab__cheap + " " + (tab === "cheap" ? styles.active : null)
+          }
+        >
+          Самый дешевый
+        </button>
+        <button
+          id="fast"
+          onClick={() => dispatch(sortTicketsByTime(state))}
+          className={
+            styles.tab__fast + " " + (tab === "fast" ? styles.active : null)
+          }
+        >
+          Самый быстрый
+        </button>
+        <button
+          id="optimal"
+          className={
+            styles.tab__optimal +
+            " " +
+            (tab === "optimal" ? styles.active : null)
+          }
+          disabled
+        >
+          Оптимальный
+        </button>
+      </div>
+    </>
   );
 };
 
